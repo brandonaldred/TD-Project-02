d35e45d42e5c33a913eacd8c50466ea430064c27
Brandon Aldred <brandonaldred@gmail.com>
2021-03-11 09:23:46 -0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -125,8 +125,7 @@ searchInput.addEventListener('keydown', (e) => {
 //Function that preforms the search and pulls out the matching information and stores it in it's own array.
 function searchData(searchQuery) {
    clearPage();
-   const searchQuery = document.getElementById('search');
-   searchData(searchQuery.value);
+   searchData(searchInput.value);
    const searchResult = [];
    searchQuery = searchQuery.toLowerCase();
    for (let i = 0; i < data.length; i++) {
